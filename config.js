22cfd6e229cdaebb9629bcbd5079bc1516200e63
Rakesh Gupta <grakesh18@gmail.com>
2016-01-04 16:11:24 +0530

    JwtRestAPI
    Building Rest API using MongoDB, Express and Node.JS. It uses JWT (JSON
    Web Token) to generate and validate token. Super cool and easy.

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret': 'ilovetolearnnewtech',
+    'database': 'mongodb://localhost/jwtdb'
+};
---
+++ server.js
@@ -0,0 +1,197 @@
+// =======================
+// get the packages we need ============
+// =======================
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var apiRoutes   = express.Router(); 
+
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+var User   = require('./models/user'); // get our mongoose model
+
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd###90876q'; 
+
+/*
+var session = require('express-session'),
+RedisStore = require('connect-redis')(session);
+
+app.use(session({
+                  store: new RedisStore(),
+                  secret: 'keyboard cat'
+}));    
+
+*/
+
+if(app.get('env') == "production")
+  console.log("Production");
+else
+  console.log('Development');
+
+// =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+mongoose.connect(config.database); // connect to database
+app.set('superSecret', config.secret); // secret variable
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// =======================
+// routes ================
+// =======================
+// basic route
+app.get('/', function(req, res) {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+
+app.get('/setup', function(req, res) {
+
+  // create a sample user
+  var nick = new User({ 
+    name: 'Rakesh Gupta', 
+    password: encrypt('demo@123456'),
+    admin: true 
+  });
+
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+
+// =================================================================================================
+// Authenticate
+// =================================================================================================
+
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
+  // find the user
+  console.log(req.headers.name);
+  User.findOne({
+    name: req.headers.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != encrypt(req.headers.password)) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign({ _id: user._id }, app.get('superSecret'), {
+          expiresIn: 1440 // expires in 24 hours
+        });
+
+
+        token = encrypt(token);
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+    }
+
+  });
+});
+
+
+// ======================================================================================================
+// route middleware to verify a token
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  var isValid = true;
+  // decode token
+  if (token) {
+	  	try{
+	  		token = decrypt(token);
+	  	}
+	  	catch(ex) {
+	  		return res.json({ success: false, message: 'Failed to authenticate token.' });
+	    }
+
+	    // verifies secret and checks exp
+	    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+	      if (err) {
+	        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+	      } else {
+	        // if everything is good, save to request for use in other routes
+	        req.decoded = decoded;    
+	        next();
+	      }
+	    });
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
+
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});  
+
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+ 
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+
+// =======================
+// start the server ======
+// =======================
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
+
